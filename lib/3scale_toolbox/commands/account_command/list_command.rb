4025a4ab585e74e77b176d6c2e385e90f35244be
f0npsje <david.carrasco@seat.es>
2021-08-16 11:54:12 +0200

    Update list accounts to improve the output

--- lib/3scale_toolbox/commands/account_command/list_command.rb
+++ lib/3scale_toolbox/commands/account_command/list_command.rb
@@ -11,7 +11,7 @@ module ThreeScaleToolbox
           def self.command
             Cri::Command.define do
               name        'list'
-              usage       'list'
+              usage       'list <remote>'
               summary     'list accounts'
               description 'List all accounts'
 
@@ -23,8 +23,20 @@ module ThreeScaleToolbox
             end
           end
 
+#{"id"=>4, "created_at"=>"2021-04-15T06:21:55Z", "updated_at"=>"2021-04-16T06:21:28Z", "credit_card_stored"=>false, "monthly_billing_enabled"=>true, "monthly_charging_enabled"=>true, "state"=>"approved", "links"=>[{"rel"=>"self", "href"=>"https://3scale-admin.3scale.apps.delorean.globalshared-dev.seat.cloud.vwgroup.com/admin/api/accounts/4"}, {"rel"=>"users", "href"=>"https://3scale-admin.3scale.apps.delorean.globalshared-dev.seat.cloud.vwgroup.com/admin/api/accounts/4/users"}], "org_name"=>"VWFS/gdpr"#}
+
+  	  ACCOUNTS_FIELDS_TO_SHOW = %w[ id state created_at org_name ]
+
           def run
-    		puts threescale_client(arguments[:remote]).list_accounts
+		  accounts = threescale_client(arguments[:remote]).list_accounts
+		  printer.print_collection accounts
+
+    #		puts threescale_client(arguments[:remote]).list_accounts
+          end
+	  
+	  def printer
+            # keep backwards compatibility
+            options.fetch(:output, CLI::CustomTablePrinter.new(ACCOUNTS_FIELDS_TO_SHOW))
           end
         end
       end
